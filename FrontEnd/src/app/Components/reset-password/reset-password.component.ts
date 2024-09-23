878e1f225f0641e04d9b40176509520622103365
mariaHany334 <mariahany334@gmail.com>
2024-09-23 14:49:09 +0300

    add resetPassword,quiz,update course pages

--- FrontEnd/src/app/Components/reset-password/reset-password.component.ts
+++ FrontEnd/src/app/Components/reset-password/reset-password.component.ts
@@ -13,5 +13,41 @@ import { HttpErrorResponse } from '@angular/common/http';
   styleUrl: './reset-password.component.css'
 })
 export class ResetPasswordComponent {
+  constructor(private _AuthService: AuthService ,private _Router:Router) { }
+  msgError:string='';
+  isLoading:boolean=false;
+
+  resetpassword: FormGroup = new FormGroup({
+   
+    newpassword: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+    repassword: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+  });
+
  
+  handleForm() {
+    
+    if (this.resetpassword.valid ) {
+      this.isLoading=true;
+      const formData = new FormData();
+     
+      formData.append('newpassword', this.resetpassword.get('newpassword')?.value);
+      formData.append('repassword', this.resetpassword.get('repassword')?.value);
+      this._AuthService.setreset(formData).subscribe({
+        next: (response) => {
+          console.log(response)
+          if (response.success){
+            this.isLoading=false;
+            //  localStorage.setItem('eToken',response.access_token);
+            //  this._AuthService.saveUserData();
+            //  this._Router.navigate(['/home'])
+          } 
+        },
+        error: (err:HttpErrorResponse) => {
+          this.isLoading=false;
+          this.msgError=err.error.message
+          console.log(err.error.message);
+        }
+      });
+    }
+}
 }
