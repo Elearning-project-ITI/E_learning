99ca583d1f414c5ed791fb1ea6f24192d7c05f65
meray hosam <meray.hosam2000@gmail.com>
2024-09-23 15:01:34 +0300

    add forgetpassword,Reviews,view course details pages

--- FrontEnd/src/app/Components/forget-password/forget-password.component.ts
+++ FrontEnd/src/app/Components/forget-password/forget-password.component.ts
@@ -13,5 +13,38 @@ import { HttpErrorResponse } from '@angular/common/http';
   styleUrl: './forget-password.component.css'
 })
 export class ForgetPasswordComponent {
-  
+  constructor(private _AuthService: AuthService ,private _Router:Router) { }
+  msgError:string='';
+  isLoading:boolean=false;
+
+  forgetpassword: FormGroup = new FormGroup({
+   
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    
+  });
+
+ 
+  handleForm() {
+    
+    if (this.forgetpassword.valid ) {
+      this.isLoading=true;
+      const formData = new FormData();
+      formData.append('email', this.forgetpassword.get('email')?.value);
+      
+ 
+      this._AuthService.setforget(formData).subscribe({
+        next: (response) => {
+          console.log(response)
+          if (response.success){
+            this.isLoading=false;
+          } 
+        },
+        error: (err:HttpErrorResponse) => {
+          this.isLoading=false;
+          this.msgError=err.error.message
+          console.log(err.error.message);
+        }
+      });
+    }
+}
 }
