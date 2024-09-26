802517e9d5466446b1328f19359d5cbca1be6a5f
meray hosam <meray.hosam2000@gmail.com>
2024-09-26 14:54:50 +0300

    CRUD admin material

---
+++ FrontEnd/src/app/Components/adminmaterial/adminmaterial.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adminmaterial',
+  standalone: true,
+  imports: [],
+  templateUrl: './adminmaterial.component.html',
+  styleUrl: './adminmaterial.component.css'
+})
+export class AdminmaterialComponent {
+
+}
