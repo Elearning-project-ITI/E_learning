bb0e75344831fc8e14d41ef342c2ce3c47d76434
mariaHany334 <mariahany334@gmail.com>
2024-09-26 14:41:30 +0300

    create admin course edit,add,view

---
+++ FrontEnd/src/app/Components/view-admin-course/view-admin-course.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-admin-course',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-admin-course.component.html',
+  styleUrl: './view-admin-course.component.css'
+})
+export class ViewAdminCourseComponent {
+
+}
